bcd9d86eb724f4cb136eed1ae56e411635b1a243
masumoto_k <cheatbook_tarou@yahoo.co.jp>
2024-08-14 13:27:31 +0900

    Add: Springプロジェクト作成

---
+++ src/test/java/com/cheatbook/amazon_balance_calculation/AmazonBalanceCalculationTests.java
@@ -0,0 +1,13 @@
+package com.cheatbook.amazon_balance_calculation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmazonBalanceCalculationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
